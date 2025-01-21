bee003b8df3a5ce96e46c921d0b35103ecbd723f
Griseld <development@venueboost.io>
2025-01-21 21:12:05 +0200

    redirect based on login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect('/admin/dashboard');
+
+  // TODO: redirect based on login
+  redirect('/crm/ecommerece/dashboard');
 
   // This won't be rendered due to redirect
   return null;
