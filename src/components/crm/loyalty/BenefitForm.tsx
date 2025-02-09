b44dbae396acbd1f887902071bdc4d1e101cd4c3
Griseld <development@venueboost.io>
2025-02-09 22:36:54 +0200

    metroshop - crm

--- src/components/crm/loyalty/BenefitForm.tsx
+++ src/components/crm/loyalty/BenefitForm.tsx
@@ -9,13 +9,6 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Benefit } from '@/hooks/useBenefits';
 import { LoyaltyProgram } from "@/app/api/external/omnigateway/types/loyalty-program";
