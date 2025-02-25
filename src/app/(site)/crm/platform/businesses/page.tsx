d8c7272f8cf3c5203bd62eda86117e661c5d2178
Griseld <development@venueboost.io>
2025-02-25 23:53:54 +0200

    fix

--- src/app/(site)/crm/platform/businesses/page.tsx
+++ src/app/(site)/crm/platform/businesses/page.tsx
@@ -1,6 +1,7 @@
 // src/app/crm/platform/businesss/page.tsx
 import { Metadata } from "next"
 import BusinessesContent from "@/components/crm/business/business-content"
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
     title: "Businesses - TrackMaster CRM",
@@ -10,7 +11,18 @@ export const metadata: Metadata = {
 export default function BusinessesPage() {
     return (
         <div className="px-3">
-            <BusinessesContent />
-        </div>
-    )
-}
+       <Suspense fallback={<BusinessesLoading />}>
+          <BusinessesContent />
+        </Suspense>
+      </div>
+    );
+  }
+
+// Loading component to show while the page is loading
+function BusinessesLoading() {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-[#0A0A0A]"></div>
+      </div>
+    );
+  }
--- src/app/(site)/crm/platform/businesses/trials/page.tsx
+++ src/app/(site)/crm/platform/businesses/trials/page.tsx
@@ -1,6 +1,7 @@
 // src/app/crm/platform/businesss/trials/page.tsx
 import { Metadata } from "next"
 import BusinessesTrialsContent from "@/components/crm/business/business-trials-content"
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
     title: "Businesses - TrackMaster CRM",
@@ -10,7 +11,18 @@ export const metadata: Metadata = {
 export default function BusinessesTrialsPage() {
     return (
         <div className="px-3">
-            <BusinessesTrialsContent />
-        </div>
-    )
-}
+       <Suspense fallback={<BusinessesTrialsLoading />}>
+          <BusinessesTrialsContent />
+        </Suspense>
+      </div>
+    );
+  }
+
+// Loading component to show while the page is loading
+function BusinessesTrialsLoading() {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-[#0A0A0A]"></div>
+      </div>
+    );
+  }
