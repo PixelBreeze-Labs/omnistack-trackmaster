4b0be636dd98c5f116be09b57d19433058cb0b93
Griseld <development@venueboost.io>
2025-02-27 22:26:20 +0200

    push

---
+++ src/app/api/external/omnigateway/staffluent-dashboard.ts
@@ -0,0 +1,15 @@
+// app/api/external/omnigateway/staffluent-dashboard.ts
+import { createOmniGateway } from './index';
+import { DashboardSummaryResponse } from './types/staffluent-dashboard';
+
+export const createDashboardApi = (apiKey: string) => {
+  const api = createOmniGateway(apiKey);
+
+  return {
+    // Get dashboard summary data
+    getDashboardSummary: async () => {
+      const { data } = await api.get<DashboardSummaryResponse>('/dashboard/summary');
+      return data;
+    }
+  };
+};
