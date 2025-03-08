d9af0ebb02f32f67f8c1a3b7f2435c08c2faa500
Griseld <development@venueboost.io>
2025-03-08 23:39:11 +0200

    "use client";

--- src/components/crm/social/social-profiles/SocialProfileForm.tsx
+++ src/components/crm/social/social-profiles/SocialProfileForm.tsx
@@ -1,4 +1,6 @@
 // components/crm/social/social-profiles/SocialProfileForm.tsx
+"use client";
+
 import { useState, useEffect } from "react";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
